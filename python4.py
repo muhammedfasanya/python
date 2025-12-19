01ce66e86be6f3e97290e672d24f7f5e71996fde
muhammedfasanya <muhammedofasanya@gmail.com>
2025-12-19 13:06:18 -0330

    phython4

---
+++ python4.py
@@ -0,0 +1,28 @@
+#OPERATOR
+
+
+#OPERAND
+#print(3+4)
+
+#print(3-4)
+
+#print(3.14 + 3.7)
+
+#print(5 * 3)
+
+#print(10 // 3)
+
+#print("muhammed" + "Ict")
+
+#print("I am sorry" * 10 )
+
+#print(10 + 10.7)
+
+#print(5 ** 5)
+
+
+#print( (3 + 5) * 3)
+
+# 24
+
+#print(5 % 3)
