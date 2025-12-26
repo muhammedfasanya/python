1835fb3e688f73f2848cee0ec17305721114da3a
muhammedfasanya <muhammedofasanya@gmail.com>
2025-12-26 16:20:35 -0330

    python 2

--- python4.py
+++ python4.py
@@ -3,26 +3,25 @@
 
 #OPERAND
 #print(3+4)
-
+#output is 7
 #print(3-4)
-
+#output is-1
 #print(3.14 + 3.7)
-
+#output is6.84
 #print(5 * 3)
-
+#output is15
 #print(10 // 3)
-
+#output is3
 #print("muhammed" + "Ict")
-
-#print("I am sorry" * 10 )
-
+#output is muhammedict
+#("I am sorry" * 10 )
+#output is I am sorryI am sorryI am sorryI am sorryI am sorryI am sorryI am sorryI am sorryI am sorryI am sorry
 #print(10 + 10.7)
-
+#output is 20.7
 #print(5 ** 5)
-
-
+#output is 3125
 #print( (3 + 5) * 3)
-
+#output is 24
 # 24
-
 #print(5 % 3)
+#output is 2
