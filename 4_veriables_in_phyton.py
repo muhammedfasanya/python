9e0d486ffab37f695a3c640208e514f6472f0464
muhammedfasanya <muhammedofasanya@gmail.com>
2026-01-02 19:17:36 -0330

    real one

---
+++ 4_veriables_in_phyton.py
@@ -0,0 +1,61 @@
+#x=math.sqrt(25)
+#x
+5.0
+#x=math.sqrt(15)
+#x
+#3.8729833462207417
+#print(math.floor(2.9))
+#2
+#print(math.ceil(2.2))
+#3
+#3 ** 2
+9
+#print(math.pow(3,2))
+#9.0
+#print(math.pi)
+3.141592653589793
+#print(math.e)
+2.718281828459045
+#math.sqrt(25)
+5.0
+#m.sqrt(25)
+5.0
+#>>> x + 10
+19
+#>>> _ + y
+22
+#>>> name = 'youtube'
+#>>> name
+'youtube'
+#>>> name + ' rocks'
+'youtube rocks'
+#>>> name ' rocks'
+#>>> name[0]
+'y'
+#>>> name[6]
+'e'
+#>>> name[8]
+#>>> name[-1]
+'e'
+#>>> name[-2]
+'b'
+#>>> name[-7]
+'y'
+#>>> name[0]
+#>>> name[-1]
+#'e'
+#>>> name[-2]
+#'b'
+#>>> name[-7]
+#'y'
+#>>> name[0:2]
+#'yo'
+#>>> name[1:4]
+#'out'
+#>>> name[1:]
+#'outube'
+#>>> name[:4]
+#'yout'
+
+
+
