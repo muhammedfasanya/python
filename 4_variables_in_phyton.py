7545afeda04c69df031ce693b04d0870ee4b78fe
muhammedfasanya <muhammedofasanya@gmail.com>
2026-01-01 22:06:58 -0330

    varibles in phyton

---
+++ 4_variables_in_phyton.py
@@ -0,0 +1,43 @@
+#Arithmetic Operations in Python
+#Wrrite a pthon code for each of these and include the output
+
+#Exercise 1: Calculate the result of 24 + 16 - 5
+#Solution:24+16-5
+# 35
+
+#Exercise 2: Multiply 12 by 9 and divide the result by 3
+#Solution:12*9/3
+#  36
+
+#Exercise 3: Find the remainder when 89 is divided by 6
+#Solution: 86%6
+# 5
+
+#Exercise 4: Perform floor division of 125 by 8
+#Solution: # 125//8
+# 5
+
+#Exercise 5: Calculate 4 raised to the power of 5
+#Solution:4**5
+# 1024
+#Exercise 6: Evaluate: 20 + 15 * 2 - 10
+#Solution:  20 + 15 * 2 - 10
+# 40
+
+#Exercise 7: Evaluate using parentheses: (20 + 15) * (2 - 1)
+#Solution: (20 + 15) * (2 - 1)
+#  35
+
+#Exercise 8: Convert 135 minutes into hours and minutes
+#Solution: 135//60 gives you 2
+#Solution:  135%60 gives you 15
+#Solution:So, 135 minutes = 2 hours and 15 minutes
+
+#Exercise 9: Find the average of 10, 20, and 30
+#Average: 20.0
+
+#Exercise 10:You have 535 seconds. Use Python arithmetic to convert this into minutes and remaining seconds.
+#Hint: Use // for the minutes and % for the remaining seconds.
+#Solution:  535 // 60 = 8
+#Solution:  535 % 60 = 55
+#Solution:So, 535 seconds = 8 minutes and 55 seconds
